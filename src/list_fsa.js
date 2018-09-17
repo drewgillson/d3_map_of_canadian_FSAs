4cbc48891e971cd2e8922eafa1ad591b1927cd7a
Drew Gillson <drew@liveoutthere.com>
2018-09-17 17:02:02 -0600

    Added functions to add FSAs and list all added FSAs

---
+++ src/list_fsa.js
@@ -0,0 +1,28 @@
+import faunadb from 'faunadb'
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  return client.query(q.Paginate(q.Match(q.Ref("indexes/all_fsas"))))
+  .then((response) => {
+    const fsaRefs = response.data
+    const getAllFsaDataQuery = fsaRefs.map((ref) => {
+      return q.Get(ref)
+    })
+    return client.query(getAllFsaDataQuery).then((ret) => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(ret)
+      })
+    })
+  }).catch((error) => {
+    console.log("error", error)
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
